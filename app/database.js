f036662323d6a1a4b3c85f529a415e4c138523a9
System <none>
2017-07-28 18:09:17 +0000

    Checkpoint

---
+++ app/database.js
@@ -0,0 +1,11 @@
+var mongo = require('mongodb').MongoClient;
+var url = "mongodb://jordan:mack@ds149551.mlab.com:49551/fcc-backend"
+
+var methods = {};
+
+methods.insert = function(search){
+  mongo.connect(url, function(err, db){
+    if (err) throw err
+    db.collection('search-history').insert()
+  });
+}
--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,10 @@
 module.exports = function(app){
   
+  app.route('/')
+    .get(function(req, res) {
+		  res.sendFile(process.cwd() + '/views/index.html');
+    });
+  
   app.route('/api/:search').get(function(req,res){
     
     
