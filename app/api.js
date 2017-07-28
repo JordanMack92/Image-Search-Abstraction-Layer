55c288b1c7022c8c81e3c9422002d997c235cbcb
System <none>
2017-07-28 19:37:17 +0000

    Checkpoint

--- app/api.js
+++ app/api.js
@@ -1,26 +1,33 @@
 var https = require('https');
+var bl = require('bl');
 
 
 var methods = {};
 
 methods.pixabay = function(search){
   
+  var path = '/api/?key=6024198-b891a26ae50627de5f5a0e197&q='+search+'&image_type=photo'
   var url = 'https://pixabay.com/api/?key=6024198-b891a26ae50627de5f5a0e197&q='+search+'&image_type=photo';
   var optionsget = {
     host : 'pixabay.com', // here only the domain name
     // (no http/https !)
     port : 443,
-    path : '/api/?key=6024198-b891a26ae50627de5f5a0e197&q='+search+'&image_type=photo', // the rest of the url with parameters if needed
+    path : path, // the rest of the url with parameters if needed
     method : 'GET' // do GET
 };
   
-  var getRequest = https.request(optionsget, function(res){
-    res.on('data', function(data){
+  https.request(optionsget, function(res){
+    res.pipe(bl(function (err, data){
+        if(err){console.log(err);}
+        data = data.toString();
+        console.log(data.split("").length);
+        console.log(data);
       return data;
-    });
-  });
-  
-  getRequest.end();
+    }));
+    // res.on('data', function(data){
+    //   console.log(data);
+    // });
+  }).end();
   
   
   
