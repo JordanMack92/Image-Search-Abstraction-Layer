2e9304f7dfc406bd5c6a765ed5025396253a25be
System <none>
2017-07-28 19:26:17 +0000

    Checkpoint

--- app/api.js
+++ app/api.js
@@ -1,23 +1,42 @@
-
+var https = require('https');
 
 
 var methods = {};
 
 methods.pixabay = function(search){
   
-  require("jsdom").env("", function(err, window) {
-    if (err) throw err
-
-    var $ = require("jquery")(window);
-    var url = 'https://pixabay.com/api/?key=6024198-b891a26ae50627de5f5a0e197&q='+search+'&image_type=photo';
+  var url = 'https://pixabay.com/api/?key=6024198-b891a26ae50627de5f5a0e197&q='+search+'&image_type=photo';
+  var optionsget = {
+    host : 'pixabay.com', // here only the domain name
+    // (no http/https !)
+    port : 443,
+    path : '/api/?key=6024198-b891a26ae50627de5f5a0e197&q='+search+'&image_type=photo', // the rest of the url with parameters if needed
+    method : 'GET' // do GET
+};
   
-   $.getJSON(url, function(data){
-    return data;
+  var getRequest = https.request(optionsget, function(res){
+    res.on('data', function(data){
+      return data;
+    });
   });
-});
+  
+  getRequest.end();
   
   
   
-}
+//   require("jsdom").env("", function(err, window) {
+//     if (err) throw err
+
+//     var $ = require("jquery")(window);
+//     var url = 'https://pixabay.com/api/?key=6024198-b891a26ae50627de5f5a0e197&q='+search+'&image_type=photo';
+  
+//    $.getJSON(url, function(data){
+//     return data;
+//   });
+};
+  
+  
+  
+
 
 module.exports = methods;
